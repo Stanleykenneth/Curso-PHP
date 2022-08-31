048b4f4d4712122e4fad54d1b85d37b3997e2f7b
Stanleykenneth <kstanley_bass@hotmail.com>
2022-08-31 12:59:11 -0300

    Exercício add

---
+++ Exercicios/operadoresMatemáticos03.php
@@ -0,0 +1,20 @@
+
+<?php 
+	
+	$adicao = 17 + 13;
+	$divisao =  $adicao / 100;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Operadores Matemáticos 03</title>
+</head>
+<body>
+	<p>17 + 13 = <?=$adicao?></p>
+	<p><?=$divisao?><p>
+
+</body>
+</html>
